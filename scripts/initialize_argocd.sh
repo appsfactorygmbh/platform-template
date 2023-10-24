4f2d9412b8a606155e8a3d2cc627d6e9d56245e2
Bhaskar Gogoi <bhaskar.gogoi@appsfactory.de>
2023-10-24 09:38:37 +0200

    Added kubectl wait condition after rollout of new changes to server

--- scripts/initialize_argocd.sh
+++ scripts/initialize_argocd.sh
@@ -7,19 +7,28 @@ echo ""
 echo "Deploy ArgoCD..."
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+echo ""
 
 echo "Wait for ArgoCD to be ready..."
 kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=300s
+echo ""
 
 echo "Configure ArgoCD..."
 kubectl apply -n argocd -f cluster/argocd-no-tls.yaml
+echo ""
 
 echo "Restart ArgoCD server..."
 kubectl -n argocd rollout restart deploy/argocd-server
 kubectl -n argocd rollout status deploy/argocd-server --timeout=300s
+echo ""
+
+echo "Wait for the restarted ArgoCD server to be ready..."
+kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=300s
+echo ""
 
 echo "Create ArgoCD App of Apps..."
 kubectl -n argocd apply -f gitops/app-of-apps.yaml
+echo ""
 
 echo "ArgoCD Admin Password"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
